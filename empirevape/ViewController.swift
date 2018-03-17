9f5ffac92e6ab98395376551909510e86e263ff8
Eric Ibarra <ericibarra@eric-macbook-pro.local>
2018-03-16 22:43:16 -0700

    Initial Commit

---
+++ empirevape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  empirevape
+//
+//  Created by Eric Ibarra on 3/16/18.
+//  Copyright © 2018 Eric Ibarra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
